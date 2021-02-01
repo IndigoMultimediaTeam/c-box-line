6a41bb6ae4b1f63b137709b2fdbc11146e0a9b74
Jan Andrle <andrle.jan@centrum.cz>
2021-02-01 16:35:19 +0100

    v1.0.3 – jen drobnosti (typy, `stroke: 2`)

---
+++ src/utils/CBoxLine_static_type.sub.js
@@ -0,0 +1,5 @@
+/**
+ * Statické hodnoty `CBoxLine` … jen pomocné pro napovídání
+ * @typedef CBoxLine_static
+ * @type {HTMLElement & { constructor: { attributes: { name: string, initial: string }[], tagName: string } }}
+ */
